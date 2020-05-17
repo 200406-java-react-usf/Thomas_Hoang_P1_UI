2ab8f1e33d7e2b9e4edeb47a931c7170e2e58871
TTHoang97 <63257552+TTHoang97@users.noreply.github.com>
2020-05-16 20:05:59 -0400

    Created variables for new-reimbursements dto.

--- src/dtos/new-reimb.ts
+++ src/dtos/new-reimb.ts
@@ -1,22 +1,43 @@
 export class NewReimb {
 
-    username: string;
-    password: string;
-    first_name: string;
-    last_name: string;
-    email: string;
+    reimb_id: number;
+    amount: number;
+    submitted: string;
+    resolved: string;
+    description: string;
+    receipt: string;
+    author_first: string;
+    author_last: string;
+    resolver_first: string;
+    resolver_last: string;
+    reimb_status: string;
+    reimb_type: string;
     
     constructor(
-        username: string, 
-        password: string, 
-        first_name: string,
-        last_name: string,
-        email: string) 
+        reimb_id: number,
+        amount: number,
+        submitted: string,
+        resolved: string,
+        description: string,
+        receipt: string,
+        author_first: string,
+        author_last: string,
+        resolver_first: string,
+        resolver_last: string,
+        reimb_status: string,
+        reimb_type: string) 
     {
-        this.username = username;
-        this.password = password;
-        this.first_name = first_name;
-        this.last_name = last_name;
-        this.email = email;
+        this.reimb_id = reimb_id;
+        this.amount = amount;
+        this.submitted = submitted;
+        this.resolved = resolved;
+        this.description = description;
+        this.receipt = receipt;
+        this.author_first = author_first;
+        this.author_last = author_last;
+        this.resolver_first = resolver_first;
+        this.resolver_last = resolver_last;
+        this.reimb_status = reimb_status;
+        this.reimb_type = reimb_type;
     }
 }
